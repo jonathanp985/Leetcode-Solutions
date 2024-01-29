3e63c1a4efcced2d5ca5f3fe13abfa7df686e7c4
Jonathan Powell <chickenfye111@gmail.com>
2024-01-28 21:52:57 -0600

    Added LC 232

---
+++ Easy/LC-232-ImplementQueueUsingStacks.py
@@ -0,0 +1,29 @@
+class MyQueue:
+
+    def __init__(self):
+        self.stack = []
+        self.size = 0
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        self.size += 1
+
+    def pop(self) -> int:
+        self.size -= 1
+        return self.stack.pop(0)
+
+    def peek(self) -> int:
+        return self.stack[0]
+
+    def empty(self) -> bool:
+        if self.size:
+            return False
+        return True
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
